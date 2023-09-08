3117914baae8cdc31d3a772659acd510f47dd6f9
arvinkgc <arvinkgcabrera@gmail.com>
2023-09-08 12:13:00 +0800

    edited few lines

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Welcome to my Portfolio!\nThis website is still under construction :(\n-arvinkgc")
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
