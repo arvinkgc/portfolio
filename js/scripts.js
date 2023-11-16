44b7f3d6fb16bce40e8b3dc62ca5f565d827f2c7
arvinkgc <arvinkgcabrera@gmail.com>
2023-11-17 01:36:55 +0800

    minor edit

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed("#auto-type", {
-strings: ['UI/UX Designer.', 'Web Developer.'],
+strings: ['Web Designer.', 'Freelancer.', 'Web Developer.'],
 typeSpeed: 100,
 backSpeed: 100,
 loop: true,
