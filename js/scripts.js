114e1ef806f54568597e602060e500608c868bdb
arvinkgc <arvinkgcabrera@gmail.com>
2023-12-12 00:38:28 +0800

    minor updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed("#auto-type", {
-strings: ['Web Designer.', 'Freelancer.', 'Web Developer.'],
+strings: ['Web Developer.', 'Freelancer.', 'Web Designer.'],
 typeSpeed: 100,
 backSpeed: 100,
 loop: true,
