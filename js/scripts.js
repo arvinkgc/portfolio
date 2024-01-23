6792f7feb3bfcb8344fb5fd295f52e9f81dfa06e
arvinkgc <arvinkgcabrera@gmail.com>
2024-01-24 01:46:17 +0800

    added loader and custom cursor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,20 @@
+// Loader JS
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Show the loader initially
+  
+    // Simulate a 2-second delay before hiding the loader
+    setTimeout(function () {
+      // Hide the loader after the delay
+      document.querySelector('.loading').style.display = 'none';
+  
+      // Show the content container
+      document.getElementById('content-container').style.display = 'block';
+    },1500);
+  });
+  
+
+// Changing Text JS
 
 var typed = new Typed("#auto-type", {
 strings: ['Web Developer.', 'Freelancer.', 'Web Designer.'],
